41e40a0c5f490fe0684ef62b1452207403da37c7
Maxim Kalinin <47250225+mkworkingman@users.noreply.github.com>
2021-01-07 03:55:13 +0400

    PDF Reader's settings are set

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,24 @@
 import React from "react"
-import { Link } from "gatsby"
+import * as pdfjsLib from 'pdfjs-dist'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import PDFFile from './1984.pdf'
+
+pdfjsLib.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/2.5.207/pdf.worker.js`
+
+pdfjsLib.getDocument(PDFFile).promise.then(pdfDoc => {
+  console.log(pdfDoc)
+})
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" meta={[
-      {
-        name: `TEST`,
-        content: 'titleTest',
-      },
-      {
-        name: `TEST2`,
-        content: 'titleTest222',
-      }
-    ]}/>
+    <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <canvas id="PDF-reader" />
+    <button onClick={() => console.log(pdfjsLib)}>test</button>
   </Layout>
 )
 
