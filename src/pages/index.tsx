aaef08efb3ba6ba30c42ec7066d00a8f4ac6ed45
Maxim Kalinin <47250225+mkworkingman@users.noreply.github.com>
2021-01-09 08:29:18 +0400

    Spinner new features

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -65,6 +65,13 @@ const IndexPage = () => {
     }
   }
 
+  const Spinner = ({display = ''}): JSX.Element => (
+    <svg className={"spinner animate-spin -ml-1 mr-3 h-5 w-5 text-gray-500 " + display} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+      <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+      <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+    </svg>
+  )
+
   return (
     <Layout>
       <SEO title="Home" />
@@ -74,7 +81,7 @@ const IndexPage = () => {
               <button
                 onClick={() => changePage(false)}
                 className="changePage focus:outline-none"
-                disabled={pageNum === 1}
+                disabled={pageNum === 1 || pageRendering}
               >
                 <span className="relative z-10">Prev Page</span>
               </button>
@@ -82,18 +89,18 @@ const IndexPage = () => {
               <button
                 onClick={() => changePage(true)}
                 className="changePage focus:outline-none"
-                disabled={pdfDoc && pageNum === pdfDoc.numPages}
+                disabled={pageNum === pdfDoc.numPages || pageRendering}
               >
                 <span className="relative z-10">Next Page</span>
               </button>
           </>
-          : <svg className="spinner animate-spin -ml-1 mr-3 h-5 w-5 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-            <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-          </svg>
+          : <Spinner />
         }
       </div>
-      <canvas ref={canvas} className={"mx-auto "} />
+      <div className="flex justify-center">
+        <canvas ref={canvas} className={pageRendering ? 'hidden': ''} />
+        <Spinner display={pageRendering ? '': 'hidden'} />
+      </div>
     </Layout>
   )
 }
