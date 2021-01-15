61f339ebf0310e3dc548bea0c348851c1675cae6
Maxim Kalinin <47250225+mkworkingman@users.noreply.github.com>
2021-01-15 04:11:01 +0400

    Build commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,8 +9,8 @@ pdfjsLib.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.j
 const IndexPage = () => {
   const canvas = useRef<HTMLCanvasElement>(null)
   const [ctx, setCtx] = useState<CanvasRenderingContext2D>(null)
-  const [bookSelected, setBookSelected] = useState<string>(localStorage.getItem('current') || '')
-  const [loading, setLoading] = useState<boolean>(localStorage.getItem('current') ? true : false)
+  const [bookSelected, setBookSelected] = useState<string>('')
+  const [loading, setLoading] = useState<boolean>(true)
   
   const scale: number = 1.2
   const [pdfDoc, setPdfDoc] = useState<any>(null)
@@ -40,7 +40,8 @@ const IndexPage = () => {
   }
 
   useEffect(() => {
-    
+    setBookSelected(localStorage.getItem('current') || '')
+    setLoading(localStorage.getItem('current') ? true : false)
   }, []);
 
   useEffect(() => {
@@ -122,14 +123,16 @@ const IndexPage = () => {
     setBookSelected(value)
   }
 
-  const Spinner = ({display = ''}): JSX.Element => (
-    <svg className={"spinner animate-spin -ml-1 mr-3 h-5 w-5 text-gray-500 " + display} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+  const Spinner = (): JSX.Element => (
+    <svg className={"spinner animate-spin -ml-1 mr-3 h-5 w-5 text-gray-500 " + (!bookSelected && 'hidden')} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
       <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
       <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
     </svg>
   )
 
-  document.onkeydown = arrowAction;
+  if (typeof document !== 'undefined') {
+    document.onkeydown = arrowAction;
+  }
 
   return (
     <Layout>
@@ -178,7 +181,7 @@ const IndexPage = () => {
         <canvas ref={canvas} className={loading ? 'hidden' : ''}/>
 
         {(pdfDoc && !loading)
-          ?         <div className={(pdfDoc && !loading) ? '' : 'hidden'}>
+          ? <div className={(pdfDoc && !loading) ? '' : 'hidden'}>
           <p className="text-center font-medium">Go to:</p>
           <input
             value={goTo}
@@ -187,7 +190,7 @@ const IndexPage = () => {
             className="rounded-lg focus:outline-none border-2 border-solid border-indigo-400 w-40 px-2 py-1 focus:border-indigo-500 text-center font-medium"
           />
         </div>
-          : <Spinner display={!localStorage.getItem('current') && 'hidden'} />
+          : <Spinner />
         }
       </div>
     </Layout>
